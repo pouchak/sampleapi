0b7f649ed22a63f2095882fdb7436c4c1ac6a1c3
Evan Pouchak <pouchak@Evans-MacBook-Pro.local>
2018-03-23 01:05:36 -0700

    nginx

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');//require it
 const app = express()//constructor
 
 app.get('/',(req, res)=>(
-    res.sendStatus('now you\'ve done it!'))
+    res.sendStatus(200))
 )
 app.post('/', (req, res)=>(console.log(req.body.message))
 )
